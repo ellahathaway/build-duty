65da927cbc9e20e4f36250bf674b1df740b06ad7
Ella Hathaway <ellahathaway@microsoft.com>
2026-04-13 14:53:44 -0700

    Wire CLI to signal collection services

--- src/BuildDuty.Cli/Program.cs
+++ src/BuildDuty.Cli/Program.cs
@@ -1,8 +1,13 @@
+using BuildDuty.Cli;
 using BuildDuty.Cli.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Spectre.Console.Cli;
 
 var services = new ServiceCollection();
+
+services.AddLogging();
+services.AddSignalCollectionServices();
+services.AddAiServices();
 
 var registrar = new TypeRegistrar(services);
 var app = new CommandApp(registrar);
